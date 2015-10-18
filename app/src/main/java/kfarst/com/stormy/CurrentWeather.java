da15be4006a0bfbfe68da571560553037f29fc53
Kevin Farst <kevin.farst@zozi.com>
2015-10-18 16:44:30 -0700

    Add project to repo

---
+++ app/src/main/java/kfarst/com/stormy/CurrentWeather.java
@@ -0,0 +1,114 @@
+package kfarst.com.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by kfarst on 10/13/15.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private String mTimeZone;
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTimeInSeconds = new Date(getTime() * 1000);
+        return formatter.format(dateTimeInSeconds);
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId() {
+        // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+        int iconId;
+
+        switch(mIcon) {
+            case "clear-day": iconId = R.drawable.clear_day;
+                break;
+            case "clear-night": iconId = R.drawable.clear_night;
+                break;
+            case "rain": iconId = R.drawable.rain;
+                break;
+            case "snow": iconId = R.drawable.snow;
+                break;
+            case "sleet": iconId = R.drawable.sleet;
+                break;
+            case "wind": iconId = R.drawable.wind;
+                break;
+            case "fog": iconId = R.drawable.fog;
+                break;
+            case "cloudy": iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day": iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night": iconId = R.drawable.cloudy_night;
+                break;
+            default: iconId = R.drawable.clear_day;
+        }
+
+        return iconId;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public int getTemperature() {
+        return (int)Math.round(mTemperature);
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public int getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return (int)Math.round(precipPercentage);
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+}
